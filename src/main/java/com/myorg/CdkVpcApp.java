df15e61ef3fdb96b7660fe2bbab0b3311a96818a
catalyst0 <46091365+catalyst0@users.noreply.github.com>
2020-08-12 21:15:28 -0400

    Initial commit

---
+++ src/main/java/com/myorg/CdkVpcApp.java
@@ -0,0 +1,13 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.App;
+
+public final class CdkVpcApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        new CdkVpcStack(app, "CdkVpcStack");
+
+        app.synth();
+    }
+}
